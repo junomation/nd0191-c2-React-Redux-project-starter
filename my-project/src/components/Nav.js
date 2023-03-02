1796b6a72ac70cb5d49398ff13fdd2b360435ae2
Junomation <42828779+junomation@users.noreply.github.com>
2023-03-02 06:49:32 -0500

    fixed navigation bar so it is highlighting the activated link
    fixed navigation bar so it is highlighting the activated link
    it also supports user logout now.

--- my-project/src/components/Nav.js
+++ my-project/src/components/Nav.js
@@ -1,28 +1,67 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { setAuthedUser } from '../actions/authedUser';
+import styles from './Nav.module.css';
 
 const Nav = () => {
+  const authedUser = useSelector((state) => state.authedUser);
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const handleLogout = () => {
+    dispatch(setAuthedUser(null));
+  };
+
   return (
-    <nav className='nav'>
+    <nav className={styles.nav}>
       <ul>
         <li>
-          <Link to='/' className='active'>
+          <Link
+            to="/"
+            className={
+              location.pathname === '/' ? styles.active : styles.inactive
+            }
+          >
             Home
           </Link>
         </li>
         <li>
-          <Link to='/add'>
+          <Link
+            to="/add"
+            className={
+              location.pathname === '/add' ? styles.active : styles.inactive
+            }
+          >
             New Question
           </Link>
         </li>
         <li>
-          <Link to='/leaderboard'>
+          <Link
+            to="/leaderboard"
+            className={
+              location.pathname === '/leaderboard'
+                ? styles.active
+                : styles.inactive
+            }
+          >
             Leaderboard
           </Link>
         </li>
+        {authedUser && (
+          <li className={styles['authed-user']}>
+            <span className={styles['nav-welcome']}>Hello, {authedUser}</span>
+            <button
+              className={styles['nav-logout']}
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </li>
+        )}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
